9920c1ab03cde66d80b722d9a463fbc632dd0f2f
ValdoNeves <valdogt1@gmail.com>
2021-08-19 22:30:13 -0300

    iniciando navegacao

---
+++ src/pages/customers/index.tsx
@@ -0,0 +1,9 @@
+import LayoutWithMenu from '../../../components/layout/LayoutWithMenu/LayoutWithMenu'
+
+export default function CustomerList(){
+  return(
+    <LayoutWithMenu>
+      Lista de Clientes
+    </LayoutWithMenu>
+  )
+}
